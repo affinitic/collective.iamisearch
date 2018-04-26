3f51b6a580e040215dff9112e00d28ef078df5c9
Sebastien Sirtoli <sebastien.sirtoli@affinitic.be>
2018-04-26 16:53:03 +0200

    WIP in the menu

--- src/collective/iamisearch/viewlets/menu.py
+++ src/collective/iamisearch/viewlets/menu.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from collective.taxonomy.interfaces import ITaxonomy
 from plone import api
 from plone.app.layout.viewlets import common
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class MenuViewlet(common.ViewletBase):
-
     index = ViewPageTemplateFile('menu.pt')
 
+    def iamlink(self):
+        utility = self.get_taxonomy('collective.taxonomy.iam')
+        # WIP
+        keys = utility.data['fr'].keys()
+
+    def isearchlink(self):
+        print"bar"
+
+    def get_taxonomy(self, name):
+        portal = api.portal.get()
+        sm = portal.getSiteManager()
+        sm.queryUtility(ITaxonomy)
+        return sm.queryUtility(ITaxonomy, name=name)
