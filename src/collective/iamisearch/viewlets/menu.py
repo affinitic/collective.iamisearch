781a81f74597c563b1acf984996ba3c419370912
Sebastien Sirtoli <sebastien.sirtoli@affinitic.be>
2018-05-22 09:33:13 +0200

    WIP show only menu filled

--- src/collective/iamisearch/viewlets/menu.py
+++ src/collective/iamisearch/viewlets/menu.py
@@ -10,29 +10,40 @@ from plone.app.layout.viewlets import common
 class MenuViewlet(common.ViewletBase):
     index = ViewPageTemplateFile('menu.pt')
 
-    def generate_menu_value_by_taxonomy_level(self, taxonomy_name, target_level=1, all_path=False):
+    def generate_menu_value_by_taxonomy_level(self, taxonomy_name, target_level=1, all_path=False, is_fill=True):
         """
         :param target_level:
         :return:
         """
-        utility = self.get_taxonomy('collective.taxonomy.'+taxonomy_name)
+        utility = self.get_taxonomy('collective.taxonomy.' + taxonomy_name)
         if not utility.data:
             return None
         target_language = str(utility.getCurrentLanguage(self.request))
         taxonomy_keys = utility.inverted_data[target_language]
         targets_by_level = []
+
+        if is_fill:
+            portal_catalog = api.portal.get_tool('portal_catalog')
+            taxonomy_filled = portal_catalog.uniqueValuesFor("taxonomy_iam")
+
         for key, value in taxonomy_keys.iteritems():
             item = taxonomy_keys[key]
             targets = item[1:].split(PATH_SEPARATOR)
             if all_path:
                 if len(targets) == target_level:
-                    targets_by_level.append(targets)
+                    if is_fill and key in taxonomy_filled:
+                        targets_by_level.append(targets)
+                    else:
+                        targets_by_level.append(targets)
+
             else:
                 if len(targets) == target_level:
-                    targets_by_level.append(targets[-1])
+                    if is_fill and key in taxonomy_filled:
+                        targets_by_level.append(targets[-1])
+                    else:
+                        targets_by_level.append(targets[-1])
 
         return targets_by_level
-
 
     def isearchlink(self):
         print"bar"
