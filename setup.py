9c6e16c4d8f541448c00748606bd31eaf65f453a
Laurent Lasudry <lasudry@gmail.com>
2019-11-26 13:46:24 +0100

    Preparing release 1.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.iamisearch",
-    version="1.0.2.dev0",
+    version='1.1',
     description="Adds 'I am' and 'I search' dropdown menus in Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
